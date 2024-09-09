4f4b49f1f8fd60a9fcd63fd843a3281355b66e28
Rahul-19962208 <rahulvaikar96@gmail.com>
2024-09-09 08:28:08 +0530

    Add files via upload

---
+++ Fizzbuzz/Fizzbuzz/Models.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fizzbuzz
+{
+    public class Models
+    {
+        public class MixedDataModel
+        {
+            public object[] Values { get; set; }
+        }
+    }
+}
